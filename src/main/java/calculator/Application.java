1128dc9e8143a4a5491d864d4b982aba8843f664
sangmok0507 <baesangmok0507@gmail.com>
2025-10-20 16:35:54 +0900

    feat: 기본 구분자로 문자열 분리 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,5 +7,22 @@ public class Application {
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String s = Console.readLine();
+        int total = 0;
+
+        char[] words = s.toCharArray(); // 각 문자를 담은 배열
+        int num = 0; // 처음 분리될 숫자의 값 초기화
+
+        for (char w : words) {
+            if (w >= '0' && w <= '9') { // 숫자 추출
+                num *= 10;
+                num += w - '0';
+            } else if (w == ',' || w == ':') {  // 구분자 기준으로 숫자 분리
+                total += num;
+                num = 0;
+            }
+        }
+        total += num; // 마지막으로 분리된 숫자의 값 초기화
+
+        System.out.println("결과: " + total); // 결과 출력
     }
 }
